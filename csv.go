e34d50cb761133fe0e94c8a973c1a9b14f1c9ba4
Amir <amir.the.h.95@gmail.com>
2021-08-09 01:20:46 +0430

    Added quote csv reader

--- csv.go
+++ csv.go
@@ -1,15 +1,19 @@
 package trade_knife
 
 import (
+	"encoding/csv"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
 // Returns csv row of the candle.
 func (c *Candle) Csv(indicators ...string) (csv string) {
 	// first basic records
-	csv = fmt.Sprintf("%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%d,%d", c.Open, c.High, c.Low, c.Close, c.Volume, c.Score, c.Opentime.Unix(), c.Closetime.Unix())
+	csv = fmt.Sprintf("%s,%s,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%d,%d", c.Symbol, c.Interval, c.Open, c.High, c.Low, c.Close, c.Volume, c.Score, c.Opentime.Unix(), c.Closetime.Unix())
 
 	// get indicators values too
 	for _, indicator := range indicators {
@@ -29,7 +33,7 @@ func (q Quote) Csv(indicators ...string) (csv string) {
 		return
 	}
 	// fix the headers
-	headers := []string{"Open", "High", "Low", "Close", "Volume", "score", "Open time", "Close time"}
+	headers := []string{"Symbol", "Interval", "Open", "High", "Low", "Close", "Volume", "Score", "Open time", "Close time"}
 
 	// and also add the indicators as well
 	for _, indicator := range indicators {
@@ -58,4 +62,45 @@ func (q Quote) WriteToCsv(filename string, indicators ...string) error {
 	}
 
 	return ioutil.WriteFile(filename, []byte(q.Csv()), 0644)
+}
+
+// Read quote from csv file.
+func NewQuoteFromCsv(filename string) (*Quote, error) {
+	csvFile, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("Successfully Opened CSV file")
+	defer csvFile.Close()
+
+	csvLines, err := csv.NewReader(csvFile).ReadAll()
+	if err != nil {
+		fmt.Println(err)
+	}
+	var quote Quote
+	// "Symbol", "Interval", "Open", "High", "Low", "Close", "Volume", "Score", "Open time", "Close time"
+	for _, line := range csvLines {
+		symbol := line[0]
+		interval := Interval(line[1])
+		open, _ := strconv.ParseFloat(line[2], 64)
+		high, _ := strconv.ParseFloat(line[3], 64)
+		low, _ := strconv.ParseFloat(line[4], 64)
+		close, _ := strconv.ParseFloat(line[5], 64)
+		volume, _ := strconv.ParseFloat(line[6], 64)
+		openTimestamp, _ := strconv.ParseFloat(line[8], 64)
+		closeTimestamp, _ := strconv.ParseFloat(line[9], 64)
+		openTime := time.Unix(int64(openTimestamp), 0)
+		closeTime := time.Unix(int64(closeTimestamp), 0)
+		candle, err := NewCandle(symbol, open, high, low, close, volume, openTime, closeTime, interval, nil, nil)
+		if err != nil {
+			return nil, err
+		}
+		candle.Score, _ = strconv.ParseFloat(line[7], 64)
+
+		quote = append(quote, candle)
+	}
+
+	q := &quote
+	q.Sort()
+	return q, nil
 }
