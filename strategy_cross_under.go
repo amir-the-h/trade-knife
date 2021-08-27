9c944dd2299e75106a3c7ba657be552fcf5b7149
Amir <amir.the.h.95@gmail.com>
2021-08-27 21:22:26 +0430

    Renaming

---
+++ strategy_cross_under.go
@@ -0,0 +1,12 @@
+package trade_knife
+
+// Score candles by checking sources cross under condition on each of them.
+func (q *Quote) ScoreByCrossUnder(score float64, fastSource, slowSource Source) {
+	quote := *q
+	// loop through quote
+	for _, candle := range quote.Candles {
+		if candle.CrossedUnder(fastSource, slowSource) {
+			candle.Score += score
+		}
+	}
+}
